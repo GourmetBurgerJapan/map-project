83e6af926a74295d1ca0e8206c627ad6e6346750
GourmetBurgerJapan <burgerinfo@gourmet-burger.net>
2026-01-30 14:10:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYnVyZ2VyaW5mbyIsImEiOiJjbWs2NHJhMmswaTZiM2dvc
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v12',
-  center: [139.7, 35.6],
+  center: [139.7, 35.6], // 東京中心
   zoom: 5
 });
 
-//  GASのURLを入れる
-fetch("https://script.google.com/macros/s/AKfycbyvSTzAO563nygi8Or3rBYLvUoRs3ZRoU6-hHuGjZvfZuTmCKRicyMck4p-Curctp1k/exec")
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyvSTzAO563nygi8Or3rBYLvUoRs3ZRoU6-hHuGjZvfZuTmCKRicyMck4p-Curctp1k/exec';
+
+fetch(GAS_URL)
   .then(res => res.json())
-  .then(data => {
-    data.forEach(place => {
+  .then(stores => {
+    stores.forEach(store => {
+      // 緯度経度が無い場合はスキップ
+      if (!store.lat || !store.lng) return;
+
+      const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+        <div class="popup">
+          <strong>${store.name}</strong><br>
+          <em>${store.editor_comment}</em><br>
+          <div>住所: ${store.address}</div>
+          <div>営業時間: ${store.opening_hours}</div>
+          <div>定休日: ${store.closed_day}</div>
+          ${store.official_url ? `<a href="${store.official_url}" target="_blank">公式サイト</a>` : ''}
+        </div>
+      `);
+
       new mapboxgl.Marker()
-        .setLngLat([place.lng, place.lat])
-        .setPopup(new mapboxgl.Popup().setText(place.name))
+        .setLngLat([store.lng, store.lat])
+        .setPopup(popup)
         .addTo(map);
     });
-  });
+  })
+  .catch(err => console.error('JSON取得エラー:', err));
+
